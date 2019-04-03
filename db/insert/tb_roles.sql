5e7f1854525b684b296b4984df2a1bb9b9c89c31
Paulo andrade <source.compu@gmail.com>
2019-04-02 22:06:50 -0600

    Agregamos controladores e inyector de dependencias.

--- db/insert/tb_roles.sql
+++ db/insert/tb_roles.sql
@@ -1,5 +1,9 @@
-insert into tb_roles (name) values('Admin');
+insert into tb_roles (name) values('Develop');
 insert into tb_roles (name) values('Owner');
 insert into tb_roles (name) values('Admin');
 insert into tb_roles (name) values('Editor');
 insert into tb_roles (name) values('Customer');
+
+-- update tb_roles set name='Develop' where id_role=1;
+
+-- select * from tb_roles
---
+++ src/Controllers/DataBaseController.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Controlador para ruta api/databases
+ * 
+ * Author: Paulo Andrade
+ * Fecha actualización: 01/04/2019
+ */
+
+namespace App\Controllers;
+
+use Psr\Http\Message\ResponseInterface;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+// Conexion base de datos
+use Cake\Database\Connection;
+
+// Entitites
+use App\Entities\UserMapper;
+use App\Entities\RoleMapper;
+
+class DataBaseController
+{
+    /**
+     * @var UserMapper
+     * @var RoleMapper
+     * @var Connection
+     */
+    protected $_user;
+    protected $_role;
+    protected $_conn;
+
+    /**
+     * Constructor.
+     *
+     * @param UserMapper $user
+     * @param RoleMapper $role
+     * @param Connection $conn
+     */
+    public function __construct(UserMapper $user, RoleMapper $role, Connection $conn)
+    {
+        $this->_user = $user;
+        $this->_role = $role;
+        $this->_conn = $conn;
+    }
+
+    public function __invoke(Request $request, Response $response): ResponseInterface
+    {
+        /** @var Container $this */
+
+        $query = $this->_conn->newQuery();
+
+        // fetch all rows as array
+        $query = $query->select('*')->from('information_schema.schemata');
+        
+        $rows = $query->execute()->fetchAll('assoc') ?: [];
+
+        // return a json response
+        // return $response->withJson(['data'=>$user->getUsers()]);
+        return $response->withJson(['data'=>$this->_role->getRoles()]);
+    }
+}
